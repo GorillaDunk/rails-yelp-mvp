0ceabe1e1ba3eaf8d2f753c2296e9b199611115e
TomKim <ehdgus5289@gmail.com>
2023-05-24 15:10:57 -0400

    second challenge done

---
+++ db/migrate/20230524190832_remove_review_to_restaurant.rb
@@ -0,0 +1,5 @@
+class RemoveReviewToRestaurant < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :restaurants, :review
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_24_162414) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_24_190832) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
